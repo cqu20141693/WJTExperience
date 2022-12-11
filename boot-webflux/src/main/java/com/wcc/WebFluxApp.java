89266609746331169de62eb97c05a227200be819
wujt <cqu20141693@gmail.com>
2022-12-11 18:54:28 +0800

    feature: add parent protobuf and deploy plugin

---
+++ boot-webflux/src/main/java/com/wcc/WebFluxApp.java
@@ -0,0 +1,11 @@
+package com.wcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApp.class,args);
+    }
+}
