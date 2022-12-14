1d0811879cb26ebd2a05e6292630b49f26a1da2c
wujt <cqu20141693@gmail.com>
2022-12-14 09:17:21 +0800

    feature: add open-framework with easy-excel

---
+++ easy-excel-sample/src/main/java/com/wcc/excel/api/model/StatisticInfo.java
@@ -0,0 +1,11 @@
+package com.wcc.excel.api.model;
+
+import lombok.Data;
+
+@Data
+public class StatisticInfo {
+    private String productName;
+    private String deviceName;
+    private String alarmName;
+    private String property;
+}
