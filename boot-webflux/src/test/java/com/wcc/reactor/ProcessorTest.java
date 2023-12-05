94bd29af82caaf0d76dc3311c7759ed4b7d49941
Jiangtao.Wu <Jiangtao.Wu@geely.com>
2023-12-05 08:59:13 +0800

    feature: Flux demo

--- boot-webflux/src/test/java/com/wcc/reactor/ProcessorTest.java
+++ boot-webflux/src/test/java/com/wcc/reactor/ProcessorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import reactor.core.Disposable;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.MonoSink;
@@ -25,7 +26,9 @@ public class ProcessorTest {
                     log.info("finally {}", signalType);
                 })
 //                .subscribe();
-                .subscribe(s->{log.info("subscribe {}",s);});
+                .subscribe(s -> {
+                    log.info("subscribe {}", s);
+                });
     }
 
     @AfterAll
@@ -44,4 +47,32 @@ public class ProcessorTest {
     }
 
 
+    @Test
+    public void testA() {
+        final EmitterProcessor<String> messageProcessor = EmitterProcessor.create(false);
+        final FluxSink<String> sink = messageProcessor.sink(FluxSink.OverflowStrategy.BUFFER);
+        Disposable subscribe = messageProcessor.doOnNext(i -> {
+            if(i.equals("10")){
+                throw new RuntimeException();
+            }
+            log.info(i);
+        }).doOnCancel(()->log.error("onCancel")).subscribe();
+        for (int i = 0; i < 10; i++) {
+            sink.next(String.valueOf(i));
+        }
+        System.out.println("hasDownstreams:" + messageProcessor.hasDownstreams());
+        sink.next("tt");
+        System.out.println("isTerminated:" + messageProcessor.isTerminated());
+      // messageProcessor.onComplete();
+        System.out.println("hasDownstreams:" + messageProcessor.hasDownstreams());
+        System.out.println("isTerminated:" + messageProcessor.isTerminated());
+       sink.next("ttt");
+        System.out.println("isCancelled:" + messageProcessor.isCancelled());
+        System.out.println("isDisposed:" + messageProcessor.isDisposed());
+        subscribe.dispose();
+         System.out.println("hasDownstreams:"+messageProcessor.hasDownstreams());
+         System.out.println("isCancelled:"+messageProcessor.isCancelled());
+         System.out.println("isDisposed:"+messageProcessor.isDisposed());
+
+    }
 }
